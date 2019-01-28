290b23a72be72f45cb817affc819f309cf7391c6
Ali Ezadkhaha <43235023+hodhod212@users.noreply.github.com>
2019-01-28 13:53:42 +0100

    ändrat på wiew

--- viewHoroscope.php
+++ viewHoroscope.php
@@ -3,8 +3,8 @@ session_start();
 
 if(isset($_SESSION["usersSign"])){
 	$time = time();
-	$time_modified=date('D M Y @ H:i:s',strtotime('+1 week'));
+	$time_modified=date('D M Y @ H:i:s',strtotime('+2 year'));
     echo "<strong><p>Ditt horoskop är: " . $_SESSION["usersSign"] . "</p></strong>"." " .$_SESSION["description"];
-   echo '<br>'."köp en lot efter en vecka exact i den här tiden:".$time_modified;
+   echo '<br>'."köp en lot efter två år exact i den här tiden:".$time_modified;
 };
 ?>
